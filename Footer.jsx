acbba3ad6ddf7313a74d110cc614998dbc40c746
pooja-s-nair <84776356+pooja-s-nair@users.noreply.github.com>
2021-06-06 21:37:05 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Footer() {
+  var currYear = new Date().getFullYear();
+  return( <footer>
+    <p>copyright @ {currYear} </p>
+    </footer>
+    );
+}
+export default Footer;
---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h1>Web Development with Javascript and React.js</h1>
+      <p>It was a 7 day long Web-Dev with Javascript & React.JS bootcamp.
+        This bootcamp gave a great base to Javascript,HTML and React.JS
+       </p>
+       <p> I really enjoyed every class of this camp.Shaurya Sir was great at teching and making us understand the topics very fast.
+        Overall I enjoyed the bootcamp and I am looking forward to such amazing bootcamps.</p>
+    </div>
+  );
+}
+export default Note;
